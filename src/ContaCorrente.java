98284913c419abc8a857d5fb19ca9fe06d47b84b
luiz-eduardo-ramirez <69591674+luiz-eduardo-ramirez@users.noreply.github.com>
2025-05-07 16:03:55 -0300

    estrutura inicial

---
+++ src/ContaCorrente.java
@@ -0,0 +1,19 @@
+public class ContaCorrente {
+
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public void depositar() {
+
+    }
+
+    public void sacar() {
+
+    }
+
+    public void transferir() {
+
+    }
+
+}
